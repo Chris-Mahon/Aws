8e71b23527adffe5be47c384785797bd43ead009
Chris Mahon <chris-mahon@hotmail.com>
2014-10-30 17:24:42 +0200

    Adding Euler 2

--- python-flask/my_application/app.py
+++ python-flask/my_application/app.py
@@ -41,3 +41,15 @@ def euler1():
 	if i%3 or i%5:
 	   sum = sum + i
      print sum
+
+@app.route("/euler2")
+def euler2():
+     term1 = 1
+     term2 = 2
+     sum = 0
+
+     for i in range (0, 4000000):
+	sum = term1 + term2
+	term1 = term2
+	term2 = sum
+     return("Fibinacci up to 4000000 is ",sum)
