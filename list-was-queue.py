5edbed33451c34f2313b53241303ddf46b4a15fe
Christopher Mahon <chris-mahon@hotmail.com>
2014-11-13 16:01:53 +0000

    Adding Queue and Message creation

--- list-was-queue.py
+++ list-was-queue.py
@@ -9,8 +9,11 @@ from boto.sqs.message import Message
 from boto.sqs.connection import SQSConnection
 from boto.exception import SQSError
 
-conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AKIAIR7EH3TNSTDUCWKA', aws_secret_access_key='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')
+conn = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id='AKIAIR7EH3TNSTDUCWKA', aws_secret_access_key='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')
 
 rs = conn.get_all_queues()
+NewQueue = conn.create_queue("ItsAWholeNewQueue")
+conn.create_message(NewQueue, "This is a Message")
 for q in rs:
 	print q.id
+
